d3b7926d7f68050a129e9bf54d6713c6de954bfd
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-05-21 16:00:52 +0530

    Circle animation and CurvedAnimation bounceout

--- animation2/lib/main.dart
+++ animation2/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show pi;
 
 void main() {
   runApp(
@@ -67,14 +68,12 @@ class HalfCircleClipper extends CustomClipper<Path> {
   const HalfCircleClipper({
     required this.side,
   });
-  
+
   @override
   Path getClip(Size size) => side.toPath(size);
 
   @override
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
-
-  
 }
 
 class HomePage extends StatefulWidget {
@@ -84,7 +83,39 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _counterClockwiseRotationController;
+  late Animation<double> _counterClockwiseRotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _counterClockwiseRotationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 1,
+      ),
+    );
+
+    _counterClockwiseRotationAnimation = Tween<double>(
+      begin: 0,
+      end: -(pi / 2),
+    ).animate(
+      CurvedAnimation(
+        parent: _counterClockwiseRotationAnimation,
+        curve: Curves.bounceInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _counterClockwiseRotationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
