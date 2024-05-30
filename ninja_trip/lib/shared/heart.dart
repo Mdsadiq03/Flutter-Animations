d9b404b2dc87ecec93601fbc826ae2684a4eab15
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-05-30 14:19:52 +0530

    TweenSequence makes the heart to pop up

--- ninja_trip/lib/shared/heart.dart
+++ ninja_trip/lib/shared/heart.dart
@@ -10,6 +10,7 @@ class Heart extends StatefulWidget {
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation _colorAnimation;
+  late Animation<double> _sizeAnimation;
   bool isFav = false;
 
   @override
@@ -17,13 +18,26 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
     super.initState();
 
     _controller = AnimationController(
-      duration: Duration(milliseconds: 200),
+      duration: Duration(milliseconds: 300),
       vsync: this,
     );
 
     _colorAnimation = ColorTween(
       begin: Colors.grey[400],
       end: Colors.red,
+    ).animate(_controller);
+
+    _sizeAnimation = TweenSequence(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 35),
+          weight: 50,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 35, end: 30),
+          weight: 50,
+        ),
+      ],
     ).animate(_controller);
 
     
@@ -60,7 +74,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
           icon: Icon(
             Icons.favorite,
             color: _colorAnimation.value,
-            size: 30,
+            size: _sizeAnimation.value,
           ),
           onPressed: () {
             isFav ? _controller.reverse() : _controller.forward();
